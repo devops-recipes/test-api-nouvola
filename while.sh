1697507fbc7497ca7e245c430137155ee8fa9ed5
ambarish2012 <achitnis2012@gmail.com>
2017-04-28 00:31:04 -0700

    Delete while.sh

--- while.sh
+++
@@ -1,7 +0,0 @@
-while [[ "$STATUS" != "Success" ]]
-do
-  STATUS=$(curl -X GET -H 'Content-Type: application/json' -H "x-api: d9dde441e880ced79fd3a076d10de224" https://divecloud.nouvola.com/api/v1/test_instances/$TEST_ID --silent | jq -r ".status")
-  echo "Status of the test run is"$STATUS
-  sleep 2	
-done
-
